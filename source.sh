d5955ff6f122a70a1d41e5e5392f8392f210bc83
petroborys <petro.borys@gmail.com>
2020-01-14 22:43:35 +0200

    Add .drone.yml 10

--- source.sh
+++ source.sh
@@ -4,5 +4,11 @@ set -ex
 
 git clone https://git.openwrt.org/openwrt/openwrt.git openwrt
 
+cd openwrt
+
+git checkout v18.06.5 
+
+make distclean
+
 # git clone --branch v18.06.5 --depth 1 https://git.openwrt.org/openwrt/openwrt.git openwrt
 
