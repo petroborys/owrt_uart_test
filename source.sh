0aeb3d9b94dfa04b2b03c46ce01e1d650d24f700
petroborys <petro.borys@gmail.com>
2020-01-14 22:16:34 +0200

    Add .drone.yml 5

--- source.sh
+++ source.sh
@@ -4,5 +4,5 @@ set -ex
 
 echo "Fetching source..."
 
-git clone --branch v18.06.6 --depth 1 https://git.openwrt.org/openwrt/openwrt.git openwrt
+git clone --branch v18.06.5 --depth 1 https://git.openwrt.org/openwrt/openwrt.git openwrt
 
