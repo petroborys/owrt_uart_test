82162140a11a42b566eede8b92f06ff29d1bd8c0
petroborys <petro.borys@gmail.com>
2020-01-14 22:46:50 +0200

    Add .drone.yml 11

--- source.sh
+++ source.sh
@@ -10,5 +10,9 @@ git checkout v18.06.5
 
 make distclean
 
+./scripts/feeds update -a
+
+./scripts/feeds install -a
+
 # git clone --branch v18.06.5 --depth 1 https://git.openwrt.org/openwrt/openwrt.git openwrt
 
