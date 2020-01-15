f86bc7c0303f8f5fa9cfb4942de14b83b68ff0ec
petroborys <petro.borys@gmail.com>
2020-01-16 01:33:23 +0200

    Add .drone.yml tar

--- source.sh
+++ source.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+export SDK=openwrt-sdk-ar71xx-mikrotik_gcc-7.4.0_musl.Linux-x86_64
+
 curl -SL https://downloads.openwrt.org/snapshots/targets/ar71xx/mikrotik/$SDK.tar.xz | tar xJ
 
 mkdir openwrt-sdk-ar71xx-mikrotik_gcc-7.4.0_musl.Linux-x86_64/package/uart_test
