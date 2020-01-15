ab57a910387b49e6f5b33eccfa94cc133c0ef722
petroborys <petro.borys@gmail.com>
2020-01-15 23:31:34 +0200

    Add .drone.yml tar

--- source.sh
+++ source.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-curl -SL https://downloads.openwrt.org/snapshots/targets/ar71xx/mikrotik/openwrt-sdk-ar71xx-mikrotik_gcc-7.4.0_musl.Linux-x86_64.tar.xz | tar x
+curl -SL https://downloads.openwrt.org/snapshots/targets/ar71xx/mikrotik/openwrt-sdk-ar71xx-mikrotik_gcc-7.4.0_musl.Linux-x86_64.tar.xz | tar xJ
 
 ls
 
