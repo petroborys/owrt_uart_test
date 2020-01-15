c212cb9f69c36b2bdbe3430ddc5ffcc93a373a40
petroborys <petro.borys@gmail.com>
2020-01-15 23:37:35 +0200

    Add .drone.yml tar

--- source.sh
+++ source.sh
@@ -6,7 +6,17 @@ curl -SL https://downloads.openwrt.org/snapshots/targets/ar71xx/mikrotik/openwrt
 
 cd openwrt-sdk-ar71xx-mikrotik_gcc-7.4.0_musl.Linux-x86_64
 
-ls
+./scripts/feeds update -a
+
+./scripts/feeds install -a
+
+git clone https://github.com/petroborys/owrt_uart_test.git package/owrt_uart_test
+
+make defconfig
+
+make package/owrt_uart_test/compile
+
+ls bin/packages/*/*
 
 # cd src
 
